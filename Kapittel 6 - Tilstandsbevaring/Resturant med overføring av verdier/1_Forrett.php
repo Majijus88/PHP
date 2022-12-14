2dc7a41e848f1006aaa0f840d668c203b8d016bc
Majijus88 <marius.holtmoen@gmail.com>
2022-12-14 18:51:23 +0100

    Commit Nr.1 Helt enkel "Resturant" der gjesten kan velge mellom tre forretter, hovedretter og desserter. Valgene til bruker blir overført fra skjema til skjema via skjulte elementer. Brukers valg oppsummeres på slutten i skjema 4

---
+++ Kapittel 6 - Tilstandsbevaring/Resturant med overføring av verdier/1_Forrett.php
@@ -0,0 +1,11 @@
+
+<h3> Først må du velge en forrett</h3>
+<form action="2_Hovedrett.php" method="post">
+    <select name= "frett">
+        <option>Tørre brødskiver</option>
+        <option>Kyllingbiter med Teriyakisaus</option>
+        <option>Bruchetta med tomat</option>
+    </select>
+    <br>
+    <input type="submit" name="knapp" value="Neste" />
+</form>
